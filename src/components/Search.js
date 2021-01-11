04e91ec5b30ce457966aa01e479360cebb25c7df
Lakshmi-saimpu-17 <soujanyasaimpu588@gmail.com>
2021-01-11 10:30:31 -0500

    once

---
+++ src/components/Search.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+const Search = () => {
+    const [term , setTerm ] = useState('programming ');
+    const [results , setResults ] = useState([]);
+     console.log(results);
+    useEffect(() => {
+        const search = async () => {
+              const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+                   params: {
+                       action : 'query',
+                       list : 'search',
+                       origin : '*',
+                       format : 'json',
+                       srsearch : term,
+                   },
+               });
+               setResults(data.query.search);
+        };
+
+        if(term && !results.length) {
+               search();
+        } else {
+             const timeoutId = setTimeout( () => {
+                search();
+               }, 1000 );
+
+        return () => {
+            clearTimeout(timeoutId);
+          }
+        } 
+        }, [term] )
+  
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key = {results.pageid } className="item">
+               <div className="right floated content">
+                   <a className="ui button"
+                         href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                      >Go!</a>
+               </div>
+                <div className="content">
+                    <div className="header">
+                        { result.title }
+                    </div>
+                    <span dangerouslySetInnerHTML = {{ __html: result.snippet}}></span>
+
+                </div>
+            </div>
+        )
+    });
+     return (
+          <div>
+            <div className="ui form">
+              <div className="field">
+                <label>Enter Search term:</label>
+                <input 
+                      value = { term }
+                      onChange = { e => setTerm(e.target.value)}
+                      className="input" />
+            </div>
+        </div>
+        <div className="ui called list">{renderedResults}</div>
+    </div>
+    );
+};
+
+
+export default Search;
